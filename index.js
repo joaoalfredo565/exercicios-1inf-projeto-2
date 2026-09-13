132d720ca040de8d93f34b95f7d760168bac80aa
João Alfredo <joao.guimaraes30@aluno.ce.gov.br>
2026-09-13 11:31:56 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+const cliente = "Lucas Almeida"
+const produto = "Notebook Gamer"
+const preco = 4500
+const quantidade = 2
+const estoque = 10
+const valorPago = 9000
+const subtotal = preco * quantidade
+
+if(quantidade<=estoque) {
+   estoqueDisponivel = "Sim"
+} else {
+    estoqueDisponivel = "estoque insuficiente"
+}
+let descontoPercentual = 0
+if(subtotal>=1000) {
+   descontoPercentual = 10
+} else {
+    descontoPercentual =0
+}
+const valorDesconto = (subtotal * descontoPercentual) /100
+const valorFinal = subtotal - valorDesconto
+
+if(valorPago>=valorFinal) {
+   pagamentoStatus = "Pagamento aprovado"
+   troco = valorPago - valorFinal
+} else {
+    pagamentoStatus = "pagamento insuficiente"
+    troco = 0
+}
+let statusPedido =  "Pedido disponível para finalização"
+if (quantidade<=estoque) {
+   estoqueDisponivel = "Sim"
+} else {
+   estoqueDisponivel = "Pedido não pode ser finalizado por falta de estoque"
+}
+const resumo = `
+cliente: ${cliente}
+produto: ${produto}
+preco: ${preco}
+quantidade: ${quantidade}
+subtotal: R${subtotal}
+desconto: R${descontoPercentual}
+valor do desconto: R${valorDesconto}
+valor final: R${valorFinal}
+situação do estoque: ${estoqueDisponivel}
+valor pago: R${valorPago}
+situação do pagamento: ${pagamentoStatus}
+troco: R${troco}
+situação do pedido: ${statusPedido}`
+console.log(resumo)
+
+module.exports = {
+cliente,
+produto,
+preco,
+quantidade,
+estoque,
+valorPago,
+subtotal,
+estoqueDisponivel,
+descontoPercentual,
+valorDesconto,
+valorFinal,
+pagamentoStatus,
+troco,
+statusPedido,
+resumo
+}
+
+
+
